9dfdecde6f782aa2f186110c51740bde1e34943a
Toby Murray <murray.toby@gmail.com>
2016-10-24 02:48:01 -0400

    Add a recipes placeholder component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { HeaderComponent } from './header.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @Component({
   selector: 'rb-root',
