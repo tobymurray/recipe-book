8cc868c814fbd071bff5407968ab709117ef0779
Toby Murray <murray.toby@gmail.com>
2016-11-19 16:15:12 -0500

    Add properties to Ingredient

--- src/app/shared/ingredient.ts
+++ src/app/shared/ingredient.ts
@@ -1,4 +1,8 @@
 export class Ingredient {
+    name: String;
+    amount: number;
     constructor(name: String, amount: number) {
+        this.name = name;
+        this.amount = amount;
     }
 }
