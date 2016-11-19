09f233f5a05b789687d52bfa18909f6036826086
Toby Murray <murray.toby@gmail.com>
2016-11-19 16:16:25 -0500

    Hook up shopping list service to recipe detail

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,3 +1,4 @@
+import { ShoppingListService } from './../../shopping-list/shopping-list.service';
 import { Component, OnInit, Input } from '@angular/core';
 
 import { Recipe } from "../recipe"
@@ -9,9 +10,13 @@ import { Recipe } from "../recipe"
 export class RecipeDetailComponent implements OnInit {
   @Input() selectedRecipe: Recipe;
 
-  constructor() { }
+  constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
+  }
+
+  onAddToShoppingList() {
+    this.shoppingListService.addItems(this.selectedRecipe.ingredients);
   }
 
 }
