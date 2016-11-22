d6a77f1c55604463137e73f12bd039df58b5e20f
Toby Murray <murray.toby@gmail.com>
2016-11-21 19:53:07 -0500

    Hook up item editing

--- src/app/shopping-list/shopping-list-add.component.ts
+++ src/app/shopping-list/shopping-list-add.component.ts
@@ -10,21 +10,23 @@ export class ShoppingListAddComponent implements OnChanges {
   @Input() item: Ingredient;
   isAdd = true;
 
-  constructor(private shoppingListService: ShoppingListService) {}
+  constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnChanges(changes) {
     if (changes.item.currentValue === null) {
       this.isAdd = true;
+      this.item = { name: null, amount: null };
     } else {
       this.isAdd = false;
     }
   }
 
   onSubmit(ingredient: Ingredient) {
+    const newIngredient = new Ingredient(ingredient.name, ingredient.amount);
     if (!this.isAdd) {
-
+      this.shoppingListService.editItem(this.item, newIngredient);
     } else {
-      this.item = new Ingredient(ingredient.name, ingredient.amount);
+      this.item = newIngredient;
       this.shoppingListService.addItem(this.item);
     }
   }
