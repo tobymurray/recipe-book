bf900364d140be1848e52871a65fd32b53b9cfb1
Toby Murray <murray.toby@gmail.com>
2016-11-21 20:29:16 -0500

    Add route subscription to give recipe edit access to recipe ID

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,14 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs/Rx';
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { RecipeService } from './../recipe.service';
 
 @Component({
   selector: 'rb-recipe-edit',
   templateUrl: './recipe-edit.component.html'
 })
-export class RecipeEditComponent implements OnInit {
+export class RecipeEditComponent implements OnInit, OnDestroy {
+  private subscription: Subscription;
+  private recipeIndex: Number;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private recipeService: RecipeService) { }
 
   ngOnInit() {
+    let isNew = true;
+    this.subscription = this.activatedRoute.params.subscribe(
+      (params: any) => {
+        if (params.hasOwnProperty('id')) {
+          isNew = false;
+          this.recipeIndex = +params['id'];
+        } else {
+          isNew = true;
+        }
+      }
+    );
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
